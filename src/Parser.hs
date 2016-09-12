ec493c72eaef38620d97acd6ba7a604649210441
Bruno Cadorette <brunocadorette@outlook.com>
2016-09-12 15:02:13 -0400

    Function parser, starting out the lambda calculus

--- src/Parser.hs
+++ src/Parser.hs
@@ -8,15 +8,30 @@ type Constructor = String
 data ProductTypeParam = Argument String | Generic String deriving(Show)
 data ProductType = ProductType Constructor [ProductTypeParam] deriving(Show)
 
+type VariableName = String
+
+data Expr = 
+      Var VariableName
+    | Apply Expr Expr
+    | Lambda VariableName Expr
+    -- | Literal Int
+    | Function String Expr --This represent a top level function, so "id a = a" will be parsed as "Function "id" (Lambda "a" (Var "a"))". 
+                           --Maybe this should be in another datatype, since Function "a" (Function "b" ...) isn't valid
+    deriving (Show)
+
+endWithSpace :: Parser a -> Parser a
+endWithSpace parser = do
+    p <- parser
+    space
+    return p
+  
 constructorString :: Parser String
 constructorString = liftA2 (:) upperChar $ many alphaNumChar
 
 parameterParser :: Parser ProductTypeParam
-parameterParser = do
-    p <- Argument <$> constructorString <|> Generic <$> some alphaNumChar 
-    space
-    return p
-    
+parameterParser = endWithSpace $ Argument <$> constructorString <|> Generic <$> some alphaNumChar 
+
+productTypeParser :: Parser ProductType
 productTypeParser = do
     constructor <- many alphaNumChar
     space
@@ -30,3 +45,28 @@ createTypeParser = do
     space
     productTypeParser
     
+functionParser :: Parser Expr
+functionParser = do
+    functionName <- many alphaNumChar
+    space
+    param <- many alphaNumChar
+    space
+    char '='
+    space
+    expr <- expressionParser
+    return $ Function functionName (Lambda param expr)
+    
+--parathesis = between (char "(") (char ")")
+lambdaParser :: Parser Expr
+lambdaParser = do
+    char '\\'
+    param <- many alphaNumChar
+    space
+    string "->"
+    space
+    expr <- expressionParser
+    return $ Lambda param expr
+    
+expressionParser = lambdaParser <|> varParser -- <|> literal
+varParser = Var <$> many alphaNumChar
+--literal = digitChar 
